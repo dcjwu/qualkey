ae9dd5fe6053b063b49c4f1bbf032cb2878f041d
igor.abramov <igor.abramov@paybis.com>
2022-04-25 17:48:28 +0300

    Feature | Set argon hash for admin login

--- qk-processing/src/app.module.ts
+++ qk-processing/src/app.module.ts
@@ -11,6 +11,7 @@ import { PrismaModule } from "./prisma/prisma.module";
 import { UserModule } from "./user/user.module";
 
 AdminJS.registerAdapter({ Database, Resource });
+import * as argon from "argon2";
 const prisma = new PrismaClient();
 const dmmf = ((prisma as any)._dmmf as DMMFClass);
 
@@ -34,7 +35,10 @@ const dmmf = ((prisma as any)._dmmf as DMMFClass);
         authenticate: async (email: string, password: string) => {
           const user = await prisma.user.findUnique({ where: { email: email } });
           if (email !== "" && password !== "") {
-            if (user?.hash === password) {
+            if (await argon.verify(
+                user?.hash,
+                password,
+            )) {
               return Promise.resolve<CurrentAdmin>({ email: email });
             }
           }
