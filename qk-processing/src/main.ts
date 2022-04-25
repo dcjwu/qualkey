20679ab38b1cbb9de0d8aa9f4bab96bbffc722ac
AK <kkucenko.aleksejs@gmail.com>
2022-04-25 15:01:31 +0300

    Try to implement adminjs.

--- qk-processing/src/main.ts
+++ qk-processing/src/main.ts
@@ -1,11 +1,31 @@
+import AdminJSExpress from "@adminjs/express"
+import { Database, Resource } from "@adminjs/prisma"
 import { ValidationPipe } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
+import AdminJS from "adminjs"
 
 import { AppModule } from "./app.module"
+import { PrismaService } from "./prisma/prisma.service"
+
+AdminJS.registerAdapter({ Database, Resource })
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+
+  const prismaService = app.get(PrismaService)
+
+  const admin = new AdminJS({
+    resources: [{
+      resource: { model: prismaService.user, client: prismaService },
+      options: {},
+    }],
+  })
+
+  const router = AdminJSExpress.buildRouter(admin)
+  app.use(admin.options.rootPath, router)
+
   await app.listen(3333)
 }
+
 bootstrap()
