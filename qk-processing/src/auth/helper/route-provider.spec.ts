b60a87dea8e8ed6369feafee20f0919ff28d1239
Aleksejs Kucenko <71713731+dcjwu@users.noreply.github.com>
2022-04-29 00:59:20 +0300

    Chore | Unit test for route-provider class
    Chore | Unit test for route-provider class
    Co-authored-by: AK <kkucenko.aleksejs@gmail.com>

---
+++ qk-processing/src/auth/helper/route-provider.spec.ts
@@ -0,0 +1,42 @@
+import { Role, User } from "@prisma/client";
+
+import { RoleNotFoundException } from "../exceptions/role-not-found.exception";
+import { RouteProvider } from "./route-provider";
+
+describe("Route Provider Unit Tests", () => {
+  const routeProvider: RouteProvider = new RouteProvider();
+
+  const dataSet = [
+    [Role.STUDENT, "/student-dashboard"],
+    [Role.INSTITUTION_REPRESENTATIVE, "/institution-dashboard"],
+  ];
+
+  it.each(dataSet)("Should return correct redirect link", (role: Role, link: string) => {
+    const user: User = {
+      uuid: "uuid2",
+      email: "",
+      password: "",
+      role: role,
+      createdAt: undefined,
+      updatedAt: undefined,
+      firstName: "",
+      lastName: "",
+    };
+    expect(routeProvider.onLogin(user)).toEqual(link);
+  });
+
+  it("Should throw error on incorrect role", () => {
+    const user: User = {
+      uuid: "uuid2",
+      email: "",
+      password: "",
+      role: Role.ADMIN,
+      createdAt: undefined,
+      updatedAt: undefined,
+      firstName: "",
+      lastName: "",
+    };
+    expect(() => routeProvider.onLogin(user)).toThrowError(new RoleNotFoundException(user.role));
+    expect(() => routeProvider.onLogin(user)).toThrowError(`Role not found ${user.role}`);
+  });
+});
