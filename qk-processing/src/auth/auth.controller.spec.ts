25434e6a2b0d81810d1a4dc50b3a1b971bccd573
Aleksejs Kucenko <71713731+dcjwu@users.noreply.github.com>
2022-04-29 11:06:37 +0300

    Chore | Auth module cover with tests
    * unit test form route-provider class
    
    * auth controller tests
    
    * start auth service unit test
    
    Co-authored-by: AK <kkucenko.aleksejs@gmail.com>

--- qk-processing/src/auth/auth.controller.spec.ts
+++ qk-processing/src/auth/auth.controller.spec.ts
@@ -1,59 +1,72 @@
-import { Test } from "@nestjs/testing";
+import { Test, TestingModule } from "@nestjs/testing";
 import { response } from "express";
 
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";
 
-describe("AuthController", () => {
-  let authController: AuthController;
-  let spyService: AuthService;
-  
+type RegisterUser = {
+    uuid: string
+    email: string
+    createdAt: Date
+}
+
+describe("AuthController Unit Test", () => {
+  let controller: AuthController;
+
+  const mockAuthService = {
+    login: jest.fn((): string => {
+      return "/route-to-be-redirected";
+    }),
+    register: jest.fn(() => {
+      return {
+        uuid: "uuid",
+        email: "email@email.com",
+        createdAt: new Date(1651188244),
+      };
+    }),
+  };
+
   beforeEach(async () => {
-    const ApiServiceProvider = {
-      provide: AuthService,
-      useFactory: () => ({
-        login: jest.fn(() => []),
-        register: jest.fn(() => []),
-      }),
-    };
-      
-    const module = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService, ApiServiceProvider],
-    }).compile();
-    
-    authController = module.get<AuthController>(AuthController);
-    spyService = module.get<AuthService>(AuthService);
+      providers: [AuthService],
+    })
+      .overrideProvider(AuthService)
+      .useValue(mockAuthService)
+      .compile();
+
+    controller = module.get<AuthController>(AuthController);
   });
 
-  describe("/register", () => {
-    const dto = new AuthDto();
-    it("Register method should not be null", () => {
-      expect(authController.register(dto)).not.toEqual(null);
-    });
+  it("Should be defined", () => {
+    expect(controller).toBeDefined();
+  });
 
-    it("Should call register method", () => {
-      authController.register(dto);
-      expect(spyService.register).toBeDefined();
-      expect(spyService.register).toHaveBeenCalled();
-      expect(spyService.register).toHaveBeenCalledWith(dto);
-      expect(spyService.register).toHaveBeenCalledTimes(1);
+  describe("/register endpoint - unit", () => {
+    const newUser: AuthDto = {
+      email: "email@email.com",
+      password: "123",
+    };
+    const registeredUser: RegisterUser = {
+      uuid: "uuid",
+      email: "email@email.com",
+      createdAt: new Date(1651188244),
+    };
+    it("Should register a user", () => {
+      expect(controller.register(newUser)).toEqual(registeredUser);
+      expect(mockAuthService.register).toHaveBeenCalledTimes(1);
     });
   });
 
-  describe("/login", () => {
-    const dto = new AuthDto();
-    it("Login method should not be null", () => {
-      expect(authController.login(dto, response)).not.toEqual(null);
-    });
-    
-    it("Should call login method", () => {
-      authController.login(dto, response);
-      expect(spyService.login).toBeDefined();
-      expect(spyService.login).toHaveBeenCalled();
-      expect(spyService.login).toHaveBeenCalledWith(dto, response);
-      expect(spyService.login).toBeCalledTimes(1);
+  describe("/login endpoint - unit", () => {
+    const existentUser: AuthDto = {
+      email: "email@email.com",
+      password: "123",
+    };
+    it("Should login a user", () => {
+      expect(controller.login(existentUser, response)).toEqual("/route-to-be-redirected");
+      expect(mockAuthService.login).toHaveBeenCalledTimes(1);
     });
   });
 });
---
+++ qk-processing/src/auth/auth.service.spec.ts
@@ -0,0 +1,83 @@
+import { ConfigService } from "@nestjs/config";
+import { JwtService } from "@nestjs/jwt";
+import { Test, TestingModule } from "@nestjs/testing";
+import { Role, User } from "@prisma/client";
+import { response } from "express";
+
+import { PrismaService } from "../prisma/prisma.service";
+import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
+import { RouteProvider } from "./helper/route-provider";
+
+type CreateUser = {
+  uuid: string
+  email: string
+  createdAt: Date
+}
+
+describe("AuthService Unit Test", () => {
+  let service: AuthService;
+
+  const mockCreateUser: CreateUser = {
+    uuid: "uuid2",
+    email: "email@email.com",
+    createdAt: new Date(1651188244),
+  };
+  
+  const mockFindUser: User = {
+    uuid: "uuid",
+    email: "email@email.com",
+    password: "$2a$10$szluigxiqw0EjjrAULtU.OrQH9ar/aTkZttoaqwvnS0u39tcVuxG.",
+    role: Role.STUDENT,
+    createdAt: new Date(1651188244),
+    updatedAt: new Date(1651188244),
+    firstName: null,
+    lastName: null,
+  };
+
+  const mockSignToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [AuthService, ConfigService, RouteProvider, {
+        provide: PrismaService,
+        useValue: {
+          user: {
+            create: jest.fn().mockReturnValue(Promise.resolve(mockCreateUser)),
+            findUnique: jest.fn().mockReturnValue(Promise.resolve(mockFindUser)),
+          },
+        },
+      }, {
+        provide: JwtService,
+        useValue: { signAsync: jest.fn().mockReturnValue(Promise.resolve(mockSignToken)) },
+      },
+      ],
+    }).compile();
+    
+    service = module.get<AuthService>(AuthService);
+  });
+
+  it("Should be defined", () => {
+    expect(service).toBeDefined();
+  });
+
+  describe("register() - unit", () => {
+    const newUser: AuthDto = {
+      email: "email@email.com",
+      password: "password",
+    };
+    it("Should register user and return it", async () => {
+      expect(await service.register(newUser)).toEqual(mockCreateUser);
+    });
+  });
+
+  describe("login() - unit", () => {
+    const loginUser: AuthDto = {
+      email: "email@email.com",
+      password: "student",
+    };
+    it("Should login user", async () => {
+      expect(await service.login(loginUser, response)).toEqual("");
+    });
+  });
+});
