eff93ebcf1fb0e016d2951a12439f694d96fd1c2
AK <kkucenko.aleksejs@gmail.com>
2022-05-01 23:59:08 +0300

    Chore | Fix broken connection from adminjs to prisma

--- qk-processing/src/app.module.ts
+++ qk-processing/src/app.module.ts
@@ -1,22 +1,20 @@
-import { AdminModule } from "@adminjs/nestjs";
+import { AdminModule, AdminModuleOptions } from "@adminjs/nestjs";
 import { Database, Resource } from "@adminjs/prisma";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { PrismaClient } from "@prisma/client";
 import { DMMFClass } from "@prisma/client/runtime";
 import AdminJS, { CurrentAdmin } from "adminjs";
 
 import { AuthModule } from "./auth/auth.module";
 import { CredentialsModule } from "./credentials/credentials.module";
 import { PrismaModule } from "./prisma/prisma.module";
+import { PrismaService } from "./prisma/prisma.service";
 import { UserModule } from "./user/user.module";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const bcrypt = require("bcryptjs");
 
 AdminJS.registerAdapter({ Database, Resource });
-const prisma = new PrismaClient();
-const dmmf = ((prisma as any)._dmmf as DMMFClass);
 
 @Module({
   imports: [
@@ -24,38 +22,47 @@ const dmmf = ((prisma as any)._dmmf as DMMFClass);
     AuthModule,
     UserModule,
     PrismaModule,
-    AdminModule.createAdmin({
-      adminJsOptions: {
-        rootPath: "/admin",
-        resources: [
-          {
-            resource: { model: dmmf.modelMap.User, client: prisma },
-            options: {},
+    CredentialsModule,
+    AdminModule.createAdminAsync({
+      imports: [PrismaModule],
+      inject: [PrismaService],
+      useFactory: async (prisma: PrismaService): Promise<AdminModuleOptions> => {
+        const dmmf = ((prisma as any)._dmmf as DMMFClass);
+        return {
+          adminJsOptions: {
+            rootPath: "/admin",
+            resources: [
+              {
+                resource: { model: dmmf.modelMap.User, client: prisma },
+                options: {},
+              },
+            ],
           },
-        ],
-      },
-      auth: {
-        authenticate: async (email: string, password: string) => {
-          if (email !== "" && password !== "") {
-            const user = await prisma.user.findUnique({ where: { email: email } });
-            if (user && user.role === "SUPER_ADMIN") {
-              if (bcrypt.compareSync(
-                password,
-                user?.password,
-                (err, res) => {
-                  console.log(err, res);
-                },
-              )) {
-                return Promise.resolve<CurrentAdmin>({ email: email });
+          auth: {
+            authenticate: async (email: string, password: string): Promise<CurrentAdmin> => {
+              if (email !== "" && password !== "") {
+                const user = await prisma.user.findUnique({ where: { email: email } });
+                if (user && user.role === "SUPER_ADMIN") {
+                  if (bcrypt.compareSync(
+                    password,
+                    user?.password,
+                    (err, res) => {
+                      console.log(err, res);
+                    },
+                  )) {
+                    return Promise.resolve<CurrentAdmin>({ email: email });
+                  }
+                }
               }
-            }
-          }
-        },
-        cookieName: "admin",
-        cookiePassword: "admin",
+            },
+            cookieName: "admin",
+            cookiePassword: "admin",
+          },
+        };
       },
     }),
-    CredentialsModule,
+
   ],
 })
-export class AppModule {}
+export class AppModule {
+}
