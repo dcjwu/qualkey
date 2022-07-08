a2fd620fe8a59847946ca5bff20d9cbec527a8ab
AK <kkucenko.aleksejs@gmail.com>
2022-07-08 11:24:13 +0300

    Chore | Amplify config

--- qk-front/pages/_app.js
+++ qk-front/pages/_app.js
@@ -1,8 +1,20 @@
 import "../assets/styles/helpers/_reset.scss"
 import "../assets/styles/_globals.scss"
+import { Amplify } from "aws-amplify"
 import Head from "next/head"
 import NextNProgress from "nextjs-progressbar"
 import { RecoilRoot } from "recoil"
+
+Amplify.configure({
+   aws_cloud_logic_custom: [
+      {
+         name: "Qualkey-dev-env", // (required) - API Name (This name is used used in the client app to identify the API - API.get('your-api-name', '/path'))
+         endpoint: "https://api.igor-dev.com/", // (required) -API Gateway URL + environment
+         region: "eu-north-1" // (required) - API Gateway region
+      }
+   ],
+   ssr: true
+})
 
 function MyApp({ Component, pageProps }) {
    return (
