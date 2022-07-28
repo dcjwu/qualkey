cbb1ee494531cd9a6978be169918df5edb13b548
igor.abramov <igor.abramov@paybis.com>
2022-07-28 16:56:34 +0300

    Bugfix | Fix apiUrl for serverRuntimeConfig

--- qk-front/next.config.js
+++ qk-front/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
    reactStrictMode: true,
    serverRuntimeConfig: {
       // Will only be available on the server side
-      apiUrl: process.env.PROCESSING_NETWORK
+      apiUrl: process.env.NEXT_PUBLIC_PROCESSING_URL
    },
    publicRuntimeConfig: {
       // Will be available on both server and client
