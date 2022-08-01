07a26f3e30153d819e7475f10b29c33c4785b0cb
igor.abramov <igor.abramov@paybis.com>
2022-08-01 18:34:38 +0300

    Bugfix | Return docker support

--- qk-front/next.config.js
+++ qk-front/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
    reactStrictMode: true,
    serverRuntimeConfig: {
       // Will only be available on the server side
-      apiUrl: process.env.NEXT_PUBLIC_PROCESSING_URL
+      apiUrl: process.env.PROCESSING_NETWORK
    },
    publicRuntimeConfig: {
       // Will be available on both server and client
