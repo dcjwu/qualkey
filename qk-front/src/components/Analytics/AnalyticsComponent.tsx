e6b5821c44b9e1857aafc69da4ab6bd216bf9e2f
AK <kkucenko.aleksejs@gmail.com>
2022-08-30 22:33:46 +0300

    Bugfix | Fix deploy tree shaking /analytics registrars

--- qk-front/src/components/Analytics/AnalyticsComponent.tsx
+++ qk-front/src/components/Analytics/AnalyticsComponent.tsx
@@ -1,8 +1,18 @@
 import React from "react"
 
-import "chart.js/auto"
-import { Chart } from "chart.js"
-import { Doughnut, Bar } from "react-chartjs-2"
+import {
+   Chart as ChartJS,
+   CategoryScale,
+   LinearScale,
+   PointElement,
+   LineElement,
+   Title,
+   Tooltip,
+   Legend,
+   ArcElement,
+   BarElement
+} from "chart.js"
+import { Chart, Doughnut, Bar } from "react-chartjs-2"
 
 import {
    colorBlack300,
@@ -17,6 +27,18 @@ import { Heading, Text } from "@lib/components"
 import type { AnalyticsComponentType } from "@customTypes/components"
 
 import styles from "./AnalyticsComponent.module.scss"
+
+ChartJS.register(
+   BarElement,
+   ArcElement,
+   CategoryScale,
+   LinearScale,
+   PointElement,
+   LineElement,
+   Title,
+   Tooltip,
+   Legend
+)
 
 export const AnalyticsComponent: React.FC<AnalyticsComponentType> = ({ statsData }): JSX.Element => {
 
@@ -222,7 +244,7 @@ export const AnalyticsComponent: React.FC<AnalyticsComponentType> = ({ statsData
          <div>
             <div className={styles.data}>
                <Heading color="800" component="p" size="sm">
-                     Qualifications Data
+                  Qualifications Data
                </Heading>
                <div className={styles.dataWrapper}>
                   <div className={styles.dataItem}>
@@ -231,19 +253,19 @@ export const AnalyticsComponent: React.FC<AnalyticsComponentType> = ({ statsData
                      </Heading>
                      <div>
                         <svg fill="none" height="49" viewBox="0 0 48 49"
-                                width="48" xmlns="http://www.w3.org/2000/svg">
+                             width="48" xmlns="http://www.w3.org/2000/svg">
                            <path d="M37.5 42.5H10.5C10.1022 42.5 9.72064 42.342 9.43934 42.0607C9.15804 41.7794 9 41.3978 9 41V8C9 7.60218 9.15804 7.22064 9.43934 6.93934C9.72064 6.65804 10.1022 6.5 10.5 6.5H28.5L39 17V41C39 41.3978 38.842 41.7794 38.5607 42.0607C38.2794 42.342 37.8978 42.5 37.5 42.5Z"
-                                 stroke="#16A34A" strokeLinecap="round"
-                                 strokeLinejoin="round" strokeWidth="1.5"/>
+                              stroke="#16A34A" strokeLinecap="round"
+                              strokeLinejoin="round" strokeWidth="1.5"/>
                            <path d="M28.5 6.5V17H39" stroke="#16A34A" strokeLinecap="round"
-                                    strokeLinejoin="round" strokeWidth="1.5"/>
+                                 strokeLinejoin="round" strokeWidth="1.5"/>
                            <path d="M18.75 28.25L24 23L29.25 28.25" stroke="#16A34A" strokeLinecap="round"
-                                    strokeLinejoin="round" strokeWidth="1.5"/>
+                                 strokeLinejoin="round" strokeWidth="1.5"/>
                            <path d="M24 35V23" stroke="#16A34A" strokeLinecap="round"
-                                    strokeLinejoin="round" strokeWidth="1.5"/>
+                                 strokeLinejoin="round" strokeWidth="1.5"/>
                         </svg>
                         <Text color="500" component="p" size="paragraph">
-                              Qualifications Uploaded
+                           Qualifications Uploaded
                         </Text>
                      </div>
                   </div>
@@ -253,23 +275,23 @@ export const AnalyticsComponent: React.FC<AnalyticsComponentType> = ({ statsData
                      </Heading>
                      <div>
                         <svg fill="none" height="49" viewBox="0 0 48 49"
-                                width="48" xmlns="http://www.w3.org/2000/svg">
+                             width="48" xmlns="http://www.w3.org/2000/svg">
                            <path d="M12 30.5C15.3137 30.5 18 27.8137 18 24.5C18 21.1863 15.3137 18.5 12 18.5C8.68629 18.5 6 21.1863 6 24.5C6 27.8137 8.68629 30.5 12 30.5Z"
-                                 stroke="#0880CE" strokeLinecap="round"
-                                 strokeLinejoin="round" strokeWidth="1.5"/>
+                              stroke="#0880CE" strokeLinecap="round"
+                              strokeLinejoin="round" strokeWidth="1.5"/>
                            <path d="M33 44C36.3137 44 39 41.3137 39 38C39 34.6863 36.3137 32 33 32C29.6863 32 27 34.6863 27 38C27 41.3137 29.6863 44 33 44Z"
-                                 stroke="#0880CE" strokeLinecap="round"
-                                 strokeLinejoin="round" strokeWidth="1.5"/>
+                              stroke="#0880CE" strokeLinecap="round"
+                              strokeLinejoin="round" strokeWidth="1.5"/>
                            <path d="M33 17C36.3137 17 39 14.3137 39 11C39 7.68629 36.3137 5 33 5C29.6863 5 27 7.68629 27 11C27 14.3137 29.6863 17 33 17Z"
-                                 stroke="#0880CE" strokeLinecap="round"
-                                 strokeLinejoin="round" strokeWidth="1.5"/>
+                              stroke="#0880CE" strokeLinecap="round"
+                              strokeLinejoin="round" strokeWidth="1.5"/>
                            <path d="M27.9555 14.2437L17.043 21.2562" stroke="#0880CE" strokeLinecap="round"
-                                    strokeLinejoin="round" strokeWidth="1.5"/>
+                                 strokeLinejoin="round" strokeWidth="1.5"/>
                            <path d="M17.043 27.7437L27.9555 34.7561" stroke="#0880CE" strokeLinecap="round"
-                                    strokeLinejoin="round" strokeWidth="1.5"/>
+                                 strokeLinejoin="round" strokeWidth="1.5"/>
                         </svg>
                         <Text color="500" component="p" size="paragraph">
-                              Qualifications Shared
+                           Qualifications Shared
                         </Text>
                      </div>
                   </div>
@@ -279,17 +301,17 @@ export const AnalyticsComponent: React.FC<AnalyticsComponentType> = ({ statsData
                      </Heading>
                      <div>
                         <svg fill="none" height="49" viewBox="0 0 48 49"
-                                width="48" xmlns="http://www.w3.org/2000/svg">
+                             width="48" xmlns="http://www.w3.org/2000/svg">
                            <path d="M24 30.5H18V24.5L36 6.5L42 12.5L24 30.5Z" stroke="#EA580C" strokeLinecap="round"
-                                    strokeLinejoin="round" strokeWidth="1.5"/>
-                           <path d="M31.5 11L37.5 17" stroke="#EA580C" strokeLinecap="round"
-                                    strokeLinejoin="round" strokeWidth="1.5"/>
-                           <path d="M40.5 23V39.5C40.5 39.8978 40.342 40.2794 40.0607 40.5607C39.7794 40.842 39.3978 41 39 41H9C8.60218 41 8.22064 40.842 7.93934 40.5607C7.65804 40.2794 7.5 39.8978 7.5 39.5V9.5C7.5 9.10218 7.65804 8.72064 7.93934 8.43934C8.22064 8.15804 8.60218 8 9 8H25.5"
-                                 stroke="#EA580C" strokeLinecap="round"
                                  strokeLinejoin="round" strokeWidth="1.5"/>
+                           <path d="M31.5 11L37.5 17" stroke="#EA580C" strokeLinecap="round"
+                                 strokeLinejoin="round" strokeWidth="1.5"/>
+                           <path d="M40.5 23V39.5C40.5 39.8978 40.342 40.2794 40.0607 40.5607C39.7794 40.842 39.3978 41 39 41H9C8.60218 41 8.22064 40.842 7.93934 40.5607C7.65804 40.2794 7.5 39.8978 7.5 39.5V9.5C7.5 9.10218 7.65804 8.72064 7.93934 8.43934C8.22064 8.15804 8.60218 8 9 8H25.5"
+                              stroke="#EA580C" strokeLinecap="round"
+                              strokeLinejoin="round" strokeWidth="1.5"/>
                         </svg>
                         <Text color="500" component="p" size="paragraph">
-                              Qualifications Edited
+                           Qualifications Edited
                         </Text>
                      </div>
                   </div>
@@ -299,14 +321,14 @@ export const AnalyticsComponent: React.FC<AnalyticsComponentType> = ({ statsData
                      </Heading>
                      <div>
                         <svg fill="none" height="49" viewBox="0 0 48 49"
-                                width="48" xmlns="http://www.w3.org/2000/svg">
+                             width="48" xmlns="http://www.w3.org/2000/svg">
                            <path d="M37.5 11L10.5 38" stroke="#D6193D" strokeLinecap="round"
-                                    strokeLinejoin="round" strokeWidth="1.5"/>
+                                 strokeLinejoin="round" strokeWidth="1.5"/>
                            <path d="M37.5 38L10.5 11" stroke="#D6193D" strokeLinecap="round"
-                                    strokeLinejoin="round" strokeWidth="1.5"/>
+                                 strokeLinejoin="round" strokeWidth="1.5"/>
                         </svg>
                         <Text color="500" component="p" size="paragraph">
-                              Qualifications Deleted
+                           Qualifications Deleted
                         </Text>
                      </div>
                   </div>
@@ -315,25 +337,25 @@ export const AnalyticsComponent: React.FC<AnalyticsComponentType> = ({ statsData
             <div className={styles.chart}>
                <div className={styles.chartItem}>
                   <Text color="800" component="p" size="paragraph">
-                        Qualifications Activated
+                     Qualifications Activated
                   </Text>
                   <Doughnut data={dataActivated} plugins={pluginActivated}/>
                </div>
                <div className={styles.chartItem}>
                   <Text color="800" component="p" size="paragraph">
-                        Qualifications Shared
+                     Qualifications Shared
                   </Text>
                   <Doughnut data={dataShared} plugins={pluginShared}/>
                </div>
                <div className={styles.chartItem}>
                   <Text color="800" component="p" size="paragraph">
-                        Qualifications Edited
+                     Qualifications Edited
                   </Text>
                   <Doughnut data={dataEdited} plugins={pluginEdited}/>
                </div>
                <div className={styles.chartItem}>
                   <Text color="800" component="p" size="paragraph">
-                        Qualifications Withdrawn
+                     Qualifications Withdrawn
                   </Text>
                   <Doughnut data={dataWithdrawn} plugins={pluginWithdrawn}/>
                </div>
@@ -341,7 +363,7 @@ export const AnalyticsComponent: React.FC<AnalyticsComponentType> = ({ statsData
          </div>
          <div className={styles.productivity}>
             <Heading color="800" component="p" size="sm">
-                  Saved Productivity
+               Saved Productivity
             </Heading>
             <Bar data={savedTimeData}/>
          </div>
